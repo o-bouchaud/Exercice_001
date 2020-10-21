52105564672da317012cf73d09886512cc4e3648
Océane Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-10-21 22:15:00 +0200

    PlayerBehavior Script updated;

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private Vector2 stickDirection;
+
+
+    private void OnEnable()
     {
-        
+        var playerController = new PlayerController();
+        playerController.Enable();
+        playerController.Main.Move.performed += MoveOnPerformed;
+        playerController.Main.Move.canceled += MoveOnCanceled;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveOnPerformed(InputAction.CallbackContext obj)
     {
-        
+        stickDirection = obj.ReadValue<Vector2>();
+    }
+
+    private void MoveOnCanceled(InputAction.CallbackContext obj)
+    {
+        stickDirection = Vector2.zero;
+    }
+
+
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var direction = new Vector2 { x = stickDirection.x, y = 0 };
+
+        if (myRigidbody2D.velocity.sqrMagnitude < maxSpeed)
+        {
+            myRigidbody2D.AddForce(direction * speed);
+        }
+
     }
 }
