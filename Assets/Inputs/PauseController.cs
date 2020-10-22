bf49cc3388b294fb19fe5b22f088bcdf269e2959
Océane Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-10-22 23:12:10 +0200

    PauseController Input Action updated;
    PauseController Input Action fully set up;

---
+++ Assets/Inputs/PauseController.cs
@@ -0,0 +1,143 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/PauseController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PauseController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PauseController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PauseController"",
+    ""maps"": [
+        {
+            ""name"": ""Main"",
+            ""id"": ""c74e65ab-ebdb-4890-a7e9-c4f34146738f"",
+            ""actions"": [
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""d16a8800-863e-4261-a4b5-95282053bec9"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""11326ce5-a330-4284-a2ba-12db3c6b402c"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8ff07cb8-818b-4405-b126-266e9766648a"",
+                    ""path"": ""<Gamepad>/start"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Main
+        m_Main = asset.FindActionMap("Main", throwIfNotFound: true);
+        m_Main_Pause = m_Main.FindAction("Pause", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Main
+    private readonly InputActionMap m_Main;
+    private IMainActions m_MainActionsCallbackInterface;
+    private readonly InputAction m_Main_Pause;
+    public struct MainActions
+    {
+        private @PauseController m_Wrapper;
+        public MainActions(@PauseController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Pause => m_Wrapper.m_Main_Pause;
+        public InputActionMap Get() { return m_Wrapper.m_Main; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MainActions set) { return set.Get(); }
+        public void SetCallbacks(IMainActions instance)
+        {
+            if (m_Wrapper.m_MainActionsCallbackInterface != null)
+            {
+                @Pause.started -= m_Wrapper.m_MainActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnPause;
+            }
+            m_Wrapper.m_MainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
+            }
+        }
+    }
+    public MainActions @Main => new MainActions(this);
+    public interface IMainActions
+    {
+        void OnPause(InputAction.CallbackContext context);
+    }
+}
