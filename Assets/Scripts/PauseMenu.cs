4e392af011ae8eb24f1ddfa57639509555cc6b84
Océane Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-10-22 23:26:47 +0200

    PauseMenu Script updated;
    PauseMenu Script fully set up;

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void OnEnable()
     {
-        
+        var pauseController = new PauseController();
+        pauseController.Enable();
+        pauseController.Main.Pause.performed += PauseOnPerformed;
     }
 
-    // Update is called once per frame
+    private void PauseOnPerformed(InputAction.CallbackContext obj)
+    {
+        if (GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+
     void Update()
     {
-        
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
